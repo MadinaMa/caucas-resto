63183bdff9f78a68df9bdfab9bfeef124ab88605
MadinaMa <mmadinag@gmail.com>
2023-11-02 15:25:39 +0100

    created html-css-js-gsap-borstrap

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.from(".link", {opacity: 0, duration: 2, delay: 2, stagger: .6})
+
+gsap.from("h1", {opacity: 0, duration: 2, delay: .8})
+
+gsap.to(".pic3", {duration: 2, delay: 2.5, x: 100, ease: "power2.in", opacity: 0})
+
+gsap.from(".block1", {opacity: 0, duration: 2, delay: 3.5})
